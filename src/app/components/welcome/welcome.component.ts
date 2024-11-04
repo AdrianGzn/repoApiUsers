72e0cc82ba0b6f7348eed6201b404e7cc2a94d73
AdrianGzn <233318@ids.upchiapas.edu.mx>
2024-11-04 00:47:18 -0600

    terminado

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
+import { OnInit } from '@angular/core';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-welcome',
@@ -8,6 +10,16 @@ import { MenuComponent } from '../menu/menu.component';
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit{
+  name: string = ''
+
+  ngOnInit(): void {
+    let user = localStorage.getItem('user');
+    if (user) {
+      let userData: User | any = JSON.parse(user);
+      this.name = userData.name;
+      console.log(this.name);
+    }    
+  }
 
 }
